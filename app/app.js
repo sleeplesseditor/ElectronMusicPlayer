cc9cf500d917135d724179341508474cd0694e56
sleeplesseditor <josh@bryson.me.uk>
2018-10-03 11:18:39 +0100

    Component and styling update added

--- app/app.js
+++ app/app.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Search from './components/search.component';
+import Details from './components/details.component';
+import Player from './components/player.component';
+import Progress from './components/progress.component';
+import Footer from './components/footer.component';
+
 class App extends React.Component {
 
     render() {
         return (
-          <form>
-            <input type = "text" />
-            <input type = "submit" />
-          </form>
+            <div>
+                <Search />
+                <Details title={'Track title'} />
+                <Player  />
+                <Progress
+                    position={'0.3'}
+                    elapsed={'00:00'}
+                    total={'0:40'}
+                />
+                <Footer />
+            </div>
         );
     }
 }
 
-ReactDOM.render( <App/> ,
+ReactDOM.render( 
+    <App/> ,
     document.getElementById('content')
 );

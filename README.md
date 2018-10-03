53192ffbb3065a8e65aeab4ba6dcdce311b469a5
sleeplesseditor <josh@bryson.me.uk>
2018-10-03 11:53:52 +0100

    Concurrently script setup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ElectronMusicPlayer
 
-A music player app built using Electron and React.
+A music player app built using Electron and React, and can be customised to use a number of streaming service APIs.
+
+To get this up and running, follow these steps: 
+
+1) cd ElectronMusicPlayer
+2) npm install -g browserify and npm install - g watchify
+3) npm install
+4) npm run dev
--- app/app.js
+++ app/app.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Search from './components/search.component';
-import Details from './components/details.component';
-import Player from './components/player.component';
-import Progress from './components/progress.component';
-import Footer from './components/footer.component';
+import AppContainer from './containers/app.container'
 
 class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Search />
-                <Details title={'Track title'} />
-                <Player  />
-                <Progress
-                    position={'0.3'}
-                    elapsed={'00:00'}
-                    total={'0:40'}
-                />
-                <Footer />
-            </div>
+            <AppContainer />
         );
     }
 }
